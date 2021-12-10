1fd2441b20c45b8510620a39d6c40f106630342d
Hamza Bendi <hamzabendi@gmail.com>
2021-12-10 18:39:19 +0100

    modified gitignore

--- smartnotes/settings.py
+++ smartnotes/settings.py
@@ -11,16 +11,19 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h1v*%07oj+l88woef=sxaal@!!k59)wvx$9&azob-n2&f6*&@_'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
